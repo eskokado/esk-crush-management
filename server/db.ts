0a52c0616f9f6435c3fcf657184ca42cb9aefd86
Edson Shideki Kokado <eskokado@DESKTOP-ESK.localdomain>
2021-01-03 09:10:40 -0300

    Developed class DataBase

---
+++ server/db.ts
@@ -0,0 +1,22 @@
+import * as mongoose from 'mongoose'
+
+class DataBase {
+
+  private dbUrl = 'mongodb://127.0.0.1/db-esk-crush-api'
+  private dbConnection;
+
+  constructor() {}
+
+  createConnection() {
+    mongoose.connect(this.dbUrl)    
+    this.logger(this.dbUrl)
+  }
+
+  logger(uri) {
+    this.dbConnection = mongoose.connection;
+    this.dbConnection.on('connected', () => console.log('mongoose is connected'))
+    this.dbConnection.on('error', error => console.error.bind(console, 'connection error ' + error))
+  }
+}
+
+export default DataBase
