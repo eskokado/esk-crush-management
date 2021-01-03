802bad09096119f445767af2862768b1ee8cdb56
Edson Shideki Kokado <eskokado@gmail.com>
2021-01-03 10:29:59 -0300

    Developed endpoint get '/api/crushs/:id'

--- server/app.ts
+++ server/app.ts
@@ -22,6 +22,8 @@ class App {
       .get((req, res) => res.status(200).json({"result": "Hello World!"}))
     this.app.route('/api/crushs')
       .get((req, res) => this.controller.select(req, res))
+    this.app.route('/api/crushs/:id')
+      .get((req, res) => this.controller.selectOne(req, res))
   }
 
 }
--- server/controller.ts
+++ server/controller.ts
@@ -3,6 +3,7 @@ import model from './model'
 class Controller {
   constructor() {}
 
+  // select
   getCrushs() {
     return model.find({})
   }
@@ -10,6 +11,18 @@ class Controller {
   select(req, res) {
     this.getCrushs()
       .then(crushs => res.status(200).json({'result': crushs}))
+      .catch(err => res.status(400).json({'result': err}))
+  }
+
+  // selectOne
+  getCrushById(id) {
+    return model.findById(id)
+  }
+
+  selectOne(req, res) {
+    const id = { id: req.params.id }
+    this.getCrushById(id)
+      .then(crush => res.status(200).json({'result': crush}))
       .catch(err => res.status(400).json({'result': err}))
   }
 }
